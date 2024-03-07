55136513eaef0ce8bd64f638f4d79d5b9e6429e3
Vladimir Vlayco <vladaj00@gmail.com>
2024-03-08 00:35:06 +0100

    some small code tweaks

--- internal/adapters/app/api/api.go
+++ internal/adapters/app/api/api.go
@@ -1,9 +1,53 @@
 package api
 
-import "github.com/vlayco/hex-go/internal/ports"
+import (
+	"github.com/vlayco/hex-go/internal/ports"
+)
+
+// This is were our app access core logic.
+// If we change something in our core, or if we entirely swap the
+// implementation, as far as that code provides all of the methods
+// defined in arithmetics' core interface, we're all good!
 
 type Adapter struct {
 	// This adapter will contain the core adapter,
 	// since api layer will access the core of the app.
+	// DI will be used in future at this point.
 	arith ports.ArithmeticPort
+}
+
+func NewAdapter(arith ports.ArithmeticPort) *Adapter {
+	return &Adapter{arith: arith}
+}
+
+func (apiAdap Adapter) GetAddition(a, b int32) (int32, error) {
+	res, err := apiAdap.arith.Addition(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return res, nil
+}
+
+func (apiAdap Adapter) GetSubstraction(a, b int32) (int32, error) {
+	res, err := apiAdap.arith.Substraction(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return res, nil
+}
+
+func (apiAdap Adapter) GetMultiplication(a, b int32) (int32, error) {
+	res, err := apiAdap.arith.Multiplication(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return res, nil
+}
+
+func (apiAdap Adapter) GetDivision(a, b int32) (int32, error) {
+	res, err := apiAdap.arith.Division(a, b)
+	if err != nil {
+		return 0, err
+	}
+	return res, nil
 }
